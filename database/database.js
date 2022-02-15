31ffa74ccc2be8b882e0578edc7133bbd561b793
thailsonbezerra <67197647+thailsonbezerra@users.noreply.github.com>
2022-02-14 21:09:54 -0300

    change database

--- database/database.js
+++ database/database.js
@@ -1,8 +1,13 @@
 const Sequelize = require('sequelize')
 
-const connection = new Sequelize('dbdp', 'root', 'thailson2001', {
-  host: 'localhost',
-  dialect: 'mysql'
-})
+const connection = new Sequelize(
+  'heroku_ce172796d058898',
+  'bbc48c19d69d1a',
+  'e4ffb741',
+  {
+    host: 'us-cdbr-east-05.cleardb.net',
+    dialect: 'mysql'
+  }
+)
 
 module.exports = connection
